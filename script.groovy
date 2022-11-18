ff902e9c5db8bafd46c68b7394349d5daf1858f4
devopspanther <116690112+devopspanther@users.noreply.github.com>
2022-11-18 10:01:52 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,19 @@
-def buildApp(): {
-  echo 'building the application ...'
-}
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
 
-def testApp(): {
-  echo 'building the application ...'
-}
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t akodevops/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push akodevops/demo-app:jma-2.0'
+    }
+} 
 
-def deployApp(): {
-  echo 'deploying the application ...'
-}
+def deployApp() {
+    echo 'deploying the application...'
+} 
 
 return this
